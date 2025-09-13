6c0e672a8162f5a1d05f18e0cc650110b6d29c89
Dangata123 <155838354+Dangata123@users.noreply.github.com>
2025-09-13 16:35:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,15 +3,17 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
-  '/app.js'
+  '/app.js',
+  '/manifest.json'
 ];
 
 // Install event
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   console.log('Service Worker installing.');
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
@@ -19,12 +21,34 @@ self.addEventListener('install', event => {
 });
 
 // Fetch event
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+  // Remove old caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
