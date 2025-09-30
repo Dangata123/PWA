441eeb7c24fb02f442acdcfbec8eb93fdcaa859c
Dangata123 <155838354+Dangata123@users.noreply.github.com>
2025-09-30 15:10:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,26 @@
 const CACHE_NAME = 'pwa-install-demo-v1';
 const urlsToCache = [
-    'PWA/',
-    'PWA/index.html',
-    'PWA/styles.css',
-    'PWA/app.js',
-    'PWA/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-        )
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
 });
