2754ea4a8641e1e1fd9a429a3e3f10b98f1d4c21
Dangata123 <155838354+Dangata123@users.noreply.github.com>
2025-09-30 11:59:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,30 @@
+const CACHE_NAME = 'pwa-install-demo-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/manifest.json'
+];
+
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
 });
